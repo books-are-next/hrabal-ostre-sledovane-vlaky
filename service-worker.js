9d6d6a9df0b797940105c61c067a3292953abca2
gabGodiskova <gabgodiskova@gmail.com>
2021-09-16 09:38:10 +0000

    Deploying to docs from @ books-are-next/hrabal-ostre-sledovane-vlaky@a2138277c01abce325361a93767646956f312698 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-e7f85fe';
+const CACHE = 'cache-a213827';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
