da6e188b641de6a25a6c8a03d50b7743b246fc44
jan-martinek <honza.martinek@gmail.com>
2021-12-01 15:57:37 +0000

    Deploying to docs from @ books-are-next/hrabal-ostre-sledovane-vlaky@0042db4d5d661a645f7447b7316ee235e30fccee 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-6d21c51';
+const CACHE = 'cache-0042db4';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -39,7 +39,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./ostre_sledovane_vlaky_001.html","./ostre_sledovane_vlaky_002.html","./ostre_sledovane_vlaky_003.html","./ostre_sledovane_vlaky_005.html","./ostre_sledovane_vlaky_006.html","./ostre_sledovane_vlaky_007.html","./ostre_sledovane_vlaky_008.html","./ostre_sledovane_vlaky_009.html","./ostre_sledovane_vlaky_010.html","./ostre_sledovane_vlaky_012.html","./resources.html","./resources/image001_fmt.jpeg","./resources/image002_fmt.jpeg","./resources/index.xml","./resources/kocka.jpeg","./resources/obalka_ostre_sledovane_fmt.jpeg","./resources/upoutavka_eknihy_fmt.jpeg","./scripts/bundle.js","./style/style.min.css"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./ostre_sledovane_vlaky_001.html","./ostre_sledovane_vlaky_002.html","./ostre_sledovane_vlaky_003.html","./ostre_sledovane_vlaky_005.html","./ostre_sledovane_vlaky_006.html","./ostre_sledovane_vlaky_007.html","./ostre_sledovane_vlaky_008.html","./ostre_sledovane_vlaky_009.html","./ostre_sledovane_vlaky_010.html","./ostre_sledovane_vlaky_012.html","./resources.html","./scripts/bundle.js","./style/style.min.css","./resources/image001_fmt.jpeg","./resources/image002_fmt.jpeg","./resources/index.xml","./resources/obalka_ostre_sledovane_fmt.jpeg","./resources/upoutavka_eknihy_fmt.jpeg","./resources/kocka.jpeg"]));
 }
 
 self.addEventListener('fetch', e => {
